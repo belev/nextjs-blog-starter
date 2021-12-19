6c98c0d0a2ceeb7e733d030b5d51ee4bb8a92d2e
Martin Belev <martinbelev93@gmail.com>
2021-12-19 16:34:46 +0200

    add site banner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,9 +19,7 @@ const Layout = ({ children, home = false }: React.PropsWithChildren<Props>) => {
         <meta name="description" content="Learn how to build a personal website using Next.js" />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={'https://nextjs-playground-rosy.vercel.app/images/banner.png'}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
